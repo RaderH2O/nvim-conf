ee972d85614876e5d607ef21c615732e0ed1dc54
RaderH2O <raderh2ogt@gmail.com>
2025-03-27 00:59:53 +0330

    <leader>cf now toggles between then config and the session folder you were inside

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,12 +7,20 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+SESSION_DIR = vim.fn.getcwd()
+IN_CONFIG = false
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
 
 -- Opening the config
 map("n", "<leader>cf", function ()
-  local config_path = vim.fn.stdpath('config')
-  vim.fn.chdir(config_path)
-  require("telescope.builtin").find_files()
+  if not IN_CONFIG then
+    local config_path = vim.fn.stdpath('config')
+    vim.fn.chdir(config_path)
+    require("telescope.builtin").find_files()
+    IN_CONFIG = true
+  else
+    vim.fn.chdir(SESSION_DIR)
+    IN_CONFIG = false
+  end
 end, { desc = "Open the config folder" })
